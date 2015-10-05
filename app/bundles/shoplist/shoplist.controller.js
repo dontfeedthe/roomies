0366c8805498cfb064990764bdbb33b438370214
Akhilian <contact.adriensaunier@gmail.com>
2015-10-05 17:53:50 +0200

    Simple use-case of the SharedFlatService

--- app/bundles/shoplist/shoplist.controller.js
+++ app/bundles/shoplist/shoplist.controller.js
@@ -1,23 +1,110 @@
 class ShopListController {
-  constructor () {
-  	this._list = []
+  constructor (SharedFlatService) {
+
+    this._sharedFlatService = SharedFlatService;
+
+  	// this._list = []
+
+    this._list = [{
+      title: 'Tancarville',
+      bought: false,
+      price: null,
+      buyer: null,
+    }, {
+      title: 'Bibliotheque',
+      bought: false,
+      price: null,
+      buyer: null,
+    }, {
+      title: 'Enceintes',
+      bought: false,
+      price: null,
+      buyer: null,
+    }, {
+      title: 'Ciseaux',
+      bought: true,
+      price: 3.99,
+      buyer: 'Adrien',
+    }, {
+      title: 'Table',
+      bought: true,
+      price: 169.0,
+      buyer: 'Simon',
+    }, {
+      title: 'Lecteur vinyl',
+      bought: false,
+      price: null,
+      buyer: null,
+    }, {
+      title: 'Télévision',
+      bought: false,
+      price: null,
+      buyer: null,
+    }, {
+      title: 'Balance',
+      bought: false,
+      price: null,
+      buyer: null,
+    }, {
+      title: 'Plat à tarte',
+      bought: true,
+      price: 14.99,
+      buyer: 'Adrien',
+    }]
   }
 
   get list () {
   	return this._list;
   }
 
-  addNewItem () {
+  getTotal () {
+    var total = 0;
+    for(var i = 0; i < this._list.length; i++) {
+      if(this._list[i].price != null) {
+        total += this._list[i].price;
+      }
+    }
+
+    return total;
+  }
+
+  getTotalByRoommate () {
+
+    var roommates = this._sharedFlatService.getRoommates(),
+        sum = {};
+
+    for(var user in roommates) {
+      sum[user] = 0;
+    }
+
+    for(var i = 0; i < this._list.length; i++) {
+      if(this._list[i].price != null) { 
+
+        if(sum[this._list[i].buyer] == null) {
+          sum[this._list[i].buyer] = 0;
+        }
+
+        sum[this._list[i].buyer] += this._list[i].price;
+      }
+    }
+
+    return sum;
+  }
+
+  addNewItem (item) {
   	this._list.push({
-  		title: 'New ITEM',
-  		bought: false,
-  		price: 2673.56
+  		title: item.title,
+  		bought: item.bought,
+  		price: item.price,
+      buyer: item.buyer,
   	})
+
+    item = {}
   }
 
   removeItem (index) {}
 }
 
-ShopListController.$inject = []
+ShopListController.$inject = ['Roomies.SharedFlatService']
 
 export default ShopListController
