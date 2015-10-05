5db3267cc56b5bde21a7eddb2155c9bee7a57c0e
Akhilian <contact.adriensaunier@gmail.com>
2015-10-05 10:33:18 +0200

    Starting project organisation

---
+++ app/app.module.js
@@ -0,0 +1,10 @@
+'use strict'
+/* global angular */
+
+var controllersNamespace = 'Roomies.controllers'
+
+/** Loading modules */
+angular.module(controllersNamespace, [])
+// .controller()
+
+export default controllersNamespace
---
+++ app/bootstrap.js
@@ -0,0 +1,4 @@
+/* global angular */
+import { default as RoomiesModules} from './app/app.module.js'
+
+angular.bootstrap(document, [RoomiesModules])
